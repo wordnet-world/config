457db0d527a9c0285dfd433b33bd4ad5411ef155
Connor Boyle <cjblink1@gmail.com>
2019-05-14 13:02:54 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,3 +1,6 @@
+echo "Creating secrets..."
+kubectl create secret generic conductor-conf --from-file=./secrets/conductor-conf.json
+echo "Secrets created."
 echo "Applying manifests..."
 kubectl apply -f config/manifests/
 echo "Manifests applied."
