688eace0e7555ae890d2c97a8ef9598dab280a0c
Connor Boyle <cjblink1@gmail.com>
2019-05-14 13:05:53 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,4 +1,7 @@
 echo "Creating secrets..."
+ls
+ls secrets
+cat secrets/conductor-conf.json
 kubectl create secret generic conductor-conf --from-file=./secrets/conductor-conf.json
 echo "Secrets created."
 echo "Applying manifests..."
