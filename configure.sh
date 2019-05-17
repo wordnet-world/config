a1b59d4f0d754dce993c6a36ce1752cc3ec92e2a
Collin Moore <collin.moore96@gmail.com>
2019-05-16 21:56:11 -0400

    Adding dumby cypher queries to populate graph

--- configure.sh
+++ configure.sh
@@ -1,6 +1,8 @@
 echo "Creating secrets..."
 kubectl delete secret conductor-conf
 kubectl create secret generic conductor-conf --from-file=config/secrets/conductor-conf.json
+kubectl delete secret dumby-neo4j-cypher
+kubectl create secrete generic dumby-neo4j-cypher --from-file=config/secrets/dumby_neo4j.cypher
 echo "Secrets created."
 echo "Applying manifests..."
 kubectl apply -f config/manifests/
