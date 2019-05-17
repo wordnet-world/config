01a5c4b4a562260bad0d2393e277add99200a242
Collin Moore <collin.moore96@gmail.com>
2019-05-16 22:10:46 -0400

    I'm had a stroke spelling dummy apparently

--- configure.sh
+++ configure.sh
@@ -1,7 +1,8 @@
 echo "Creating secrets..."
 kubectl delete secret conductor-conf
 kubectl create secret generic conductor-conf --from-file=config/secrets/conductor-conf.json
-kubectl create secret generic dumby-neo4j-cypher --from-file=config/secrets/dumby_neo4j.cypher
+kubectl delete secret dummy-neo4j-cypher
+kubectl create secret generic dummy-neo4j-cypher --from-file=config/secrets/dummy_neo4j.cypher
 echo "Secrets created."
 echo "Applying manifests..."
 kubectl apply -f config/manifests/
