550667eac0c41beda87a8f05665fdef29f894b54
Collin Moore <collin.moore96@gmail.com>
2019-05-16 22:04:50 -0400

    need a secret to exist before I can delete it apparently

--- configure.sh
+++ configure.sh
@@ -1,7 +1,6 @@
 echo "Creating secrets..."
 kubectl delete secret conductor-conf
 kubectl create secret generic conductor-conf --from-file=config/secrets/conductor-conf.json
-kubectl delete secret dumby-neo4j-cypher
 kubectl create secrete generic dumby-neo4j-cypher --from-file=config/secrets/dumby_neo4j.cypher
 echo "Secrets created."
 echo "Applying manifests..."
