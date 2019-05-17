d665b95732349c0f156fb0ae756b6ae19fd23525
Collin Moore <collin.moore96@gmail.com>
2019-05-16 22:06:40 -0400

    misspelled secret

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 echo "Creating secrets..."
 kubectl delete secret conductor-conf
 kubectl create secret generic conductor-conf --from-file=config/secrets/conductor-conf.json
-kubectl create secrete generic dumby-neo4j-cypher --from-file=config/secrets/dumby_neo4j.cypher
+kubectl create secret generic dumby-neo4j-cypher --from-file=config/secrets/dumby_neo4j.cypher
 echo "Secrets created."
 echo "Applying manifests..."
 kubectl apply -f config/manifests/
